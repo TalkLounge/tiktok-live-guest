ffd8fc4ad692f74a4a0be1b740169b40e7466099
TalkLounge <talklounge@yahoo.de>
2023-11-10 17:32:27 +0100

    Update to 1.1.0
    - Fix Livestream Crash after Time Bug
    - Add Live Preview to Profile
    - Make Livestream Resizable

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            TikTok Live Guest
 // @name:de         TikTok Live Gast
-// @version         1.0.1
+// @version         1.1.0
 // @description     View TikTok Live without Login as Guest
 // @description:de  TikTok Live schauen ohne Login als Gast
 // @icon            https://www.tiktok.com/favicon.ico
@@ -9,45 +9,134 @@
 // @namespace       https://github.com/TalkLounge/tiktok-live-guest
 // @license         MIT
 // @match           https://www.tiktok.com/*
-// @require         https://xqq.im/mpegts.js/dist/mpegts.js
+// @require         https://unpkg.com/mpegts.js@1.7.3/dist/mpegts.js
 // @grant           none
 // ==/UserScript==
 
 // 24/7 Stream: https://www.tiktok.com/@livehaf.official/live
 (async () => {
-    if (!window.location.pathname.endsWith("/live")) {
-        return;
+    let player;
+
+    const div = document.createElement("div"); // Parent Div
+    div.style.display = "flex";
+    div.style.justifyContent = "center";
+
+    const divResize = document.createElement("div"); // Resize Video Div
+    divResize.style.resize = "vertical";
+    divResize.style.overflow = "hidden";
+    divResize.style.height = "50vh";
+    div.append(divResize);
+
+    async function removePopups() {
+        for (let i = 0; i < 10 * 4; i++) {
+            document.querySelector("#login-modal")?.parentNode?.parentNode?.parentNode?.remove(); // Remove Login Modal
+            document.querySelector("tiktok-cookie-banner")?.remove(); // Remove Cookie Banner
+            await new Promise(r => setTimeout(r, 250));
+        }
     }
 
-    const room_id = document.querySelector('[property="al:ios:url"]').getAttribute("content").split("=")[1];
-    let data = await fetch(`https://webcast.tiktok.com/webcast/room/info/?aid=1988&room_id=${room_id}`);
-    data = await data.json();
-    console.log("Stream URLs", data.data.stream_url);
-    data = data.data.stream_url.flv_pull_url;
-
-    if (!mpegts.getFeatureList().mseLivePlayback) {
-        return;
+    function initPlayer(url, video) {
+        player = mpegts.createPlayer({
+            type: "mse",
+            isLive: true,
+            url
+        });
+        player.attachMediaElement(video);
+        player.load();
+        player.play();
     }
 
-    const video = document.createElement("video");
-    video.style.height = "100%";
-    video.style.width = "100%";
-    video.controls = true;
-    document.querySelector('[role="video-container"] div:nth-child(2) div').append(video);
+    async function initLive() {
+        if (!window.location.pathname.startsWith("/@") || !window.location.pathname.endsWith("/live")) {
+            return;
+        }
 
-    const player = mpegts.createPlayer({
-        type: "mse",
-        isLive: true,
-        url: data.pm_mt_video_1080p60 || data.FULL_HD1 || data.pm_mt_video_720p60 || data.HD1 || data.SD2 || data.SD1
-    });
-    player.attachMediaElement(video);
-    player.load();
-    player.play();
+        const room_id = document.querySelector('[property="al:ios:url"]').getAttribute("content").split("=")[1];
+        let data = await fetch(`https://webcast.tiktok.com/webcast/room/info/?aid=1988&room_id=${room_id}`);
+        data = await data.json();
+        console.log("[TikTok Live Gast]: Stream URLs", data.data.stream_url);
+        data = data.data.stream_url.flv_pull_url;
+        data = data.pm_mt_video_1080p60 || data.FULL_HD1 || data.pm_mt_video_720p60 || data.HD1 || data.SD2 || data.SD1;
 
-    for (let i = 0; i <= 10 * 4; i++) {
-        document.querySelector("#login-modal")?.parentNode?.parentNode?.parentNode?.remove(); // Remove Login Modal
-        document.querySelector("tiktok-cookie-banner")?.remove(); // Remove Cookie Banner
-        document.querySelector('[role="video-container"] div:nth-child(2) div:nth-child(4)')?.remove(); // Remove Video Overlay
-        await new Promise(r => setTimeout(r, 250));
+        if (!mpegts.getFeatureList().mseLivePlayback) {
+            return;
+        }
+
+        div.style.position = "absolute";
+        div.style.width = "90%";
+        div.style.zIndex = 2;
+        div.style.marginTop = `${document.querySelector("[class*=DivLiveRoomBanner]").getBoundingClientRect().height}px`;
+
+        const parent = document.querySelector("[class*=DivLiveContainer]");
+        parent.insertBefore(div, parent.firstChild);
+
+        const video = document.createElement("video");
+        video.style.height = "100%";
+        video.controls = true;
+        video.volume = 0.5;
+
+        divResize.style.height = "75vh";
+        divResize.append(video);
+
+        document.querySelector("[class*=DivFeedLivePlayerCoreContainer]").remove();
+        document.querySelector("[class*=DivChatRoomAnimationContainer]").remove();
+
+        initPlayer(data, video);
+
+        video.addEventListener("ended", () => { // [MSEController] > MediaSource onSourceEnded
+            player.destroy();
+            initPlayer(data, video);
+        });
     }
+
+    async function initProfile() {
+        for (let i = 0; i < 10 * 4; i++) {
+            await new Promise(r => setTimeout(r, 250));
+
+            if (!window.location.pathname.startsWith("/@")) {
+                continue;
+            }
+
+            if (document.querySelector("[data-e2e=user-post-item-list] a[target=tiktok_live_view_window] video")) {
+                console.log("[TikTok Live Gast]: Detected Live in List");
+                await new Promise(r => setTimeout(r, 2500));
+
+                const parent = document.querySelector("#main-content-others_homepage");
+                parent.insertBefore(div, parent.firstChild);
+
+                const video = document.querySelector("[data-e2e=user-post-item-list] a[target=tiktok_live_view_window] video");
+                video.style.height = "100%";
+                video.style.transform = "";
+                video.controls = true;
+                video.muted = false;
+                video.volume = 0.5;
+                divResize.append(video);
+
+                break;
+            } else if (document.querySelector("#main-content-others_homepage").parentNode.firstChild.target == "tiktok_live_view_window" && document.querySelector("#main-content-others_homepage").parentNode.firstChild.querySelector("video")) {
+                console.log("[TikTok Live Gast]: Detected Live in Banner");
+                await new Promise(r => setTimeout(r, 3000));
+
+                const a = document.querySelector("a[target=tiktok_live_view_window]");
+                a.querySelector("div").style.height = "100%";
+                a.parentNode.insertBefore(div, a.parentNode.firstChild);
+
+                const video = a.querySelector("video");
+                video.style.height = "100%";
+                video.style.transform = "";
+                video.controls = true;
+                video.muted = false;
+                video.volume = 0.5;
+                divResize.append(video);
+
+                div.append(a);
+
+                break;
+            }
+        }
+    }
+
+    initLive();
+    initProfile();
+    removePopups();
 })()
